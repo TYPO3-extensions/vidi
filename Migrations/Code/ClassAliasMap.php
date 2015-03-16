9d889d57bb35dceb7bf108c18d402f30d7df03f4
Fabien Udriot <fabien.udriot@ecodev.ch>
2015-03-16 12:24:34 +0100

    [TASK] Move code to Github - https://github.com/fabarea/vidi
    Change-Id: I2639cf4044aa2761059d545aa72682c037d57e75

--- Migrations/Code/ClassAliasMap.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array(
-	'Tx_Vidi_Controller_ContentController' => 'TYPO3\\CMS\\Vidi\\Controller\\Backend\\ContentController',
-	'Tx_Vidi_Controller_FacetController' => 'TYPO3\\CMS\\Vidi\\Controller\\Backend\\FacetController',
-	'Tx_Vidi_Controller_ToolController' => 'TYPO3\\CMS\\Vidi\\Controller\\Backend\\ToolController',
-);
-?>
